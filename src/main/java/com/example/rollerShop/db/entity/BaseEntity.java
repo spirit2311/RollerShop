801338895ee5b0957f2bd0b7ac0664abb655524e
loshkarjova <loshkarjova@gmail.com>
2023-02-26 00:23:19 +0200

    added converter for uuid in BaseEntity class. pls remove fields in the others entities bcs you will extend base entity

--- src/main/java/com/example/rollerShop/db/entity/BaseEntity.java
+++ src/main/java/com/example/rollerShop/db/entity/BaseEntity.java
@@ -1,12 +1,36 @@
 package com.example.rollerShop.db.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 
+import java.util.UUID;
 
+@MappedSuperclass
 public abstract class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "UUID", nullable = false, length = 36, unique = true)
+    @Convert(converter = UUIDConverter.class)
+    private UUID uuid = UUID.randomUUID();
+
+
+    private static class UUIDConverter implements AttributeConverter<UUID, String> {
+        @Override
+        public String convertToDatabaseColumn(UUID attribute) {
+            return attribute.toString();
+        }
+
+        @Override
+        public UUID convertToEntityAttribute(String dbData) {
+            return UUID.fromString(dbData);
+        }
+    }
 }
